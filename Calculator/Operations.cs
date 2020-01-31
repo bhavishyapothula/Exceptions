11ac7738844386da4499b3aeb7feaba6e77fa9eb
Bhavishya Pothula <Bhavishya_Pothula@epam.com>
2020-01-31 14:32:46 +0530

    Add project files.

---
+++ Calculator/Operations.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculations
+    {
+        /// <summary>
+        /// Addition
+        /// </summary>
+        /// <param name="operand1"></param>
+        /// <param name="operand2"></param>
+        /// <returns>sum</returns>
+        public int Add(int operand1, int operand2)
+        {
+            return operand1 + operand2;
+        }
+        /// <summary>
+        /// Subtraction
+        /// </summary>
+        /// <param name="operand1"></param>
+        /// <param name="operand2"></param>
+        /// <returns>difference</returns>
+        public int Subtract(int operand1, int operand2)
+        {
+            return operand1 - operand2;
+        }
+        /// <summary>
+        /// Multiplication
+        /// </summary>
+        /// <param name="operand1"></param>
+        /// <param name="operand2"></param>
+        /// <returns>Product</returns>
+        public int Multiply(int operand1, int operand2)
+        {
+
+           int result=0;
+            try
+            {
+                if(operand1==0||operand2==0)
+                {
+                    //throwing exception
+                    throw new MultiplyByZeroException("Operand should not be zero");
+                }
+                result = operand1 * operand2;
+                
+            }
+            //catching exception
+            catch(MultiplyByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return result;
+        }
+        public double Divide(int operand1, int operand2)
+        {
+            double result = 0;
+            try
+            {
+                result=operand1 / operand2;
+               
+            }
+            catch (DivideByZeroException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            return result;
+        }
+    
+    
+    }
+    /// <summary>
+    /// Creating user defined exception
+    /// </summary>
+    public class MultiplyByZeroException:Exception
+    {
+        public MultiplyByZeroException():base()
+        {
+
+        }
+        public MultiplyByZeroException(String message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ Exceptions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Calculator;
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int op1, op2,ch;
+            Calculations cal = new Calculations();
+            //Reading operand1
+            Console.WriteLine("enter first number");
+            op1=Convert.ToInt32(Console.ReadLine());
+            //Reading operand2
+            Console.WriteLine("enter second number");
+            op2= Convert.ToInt32(Console.ReadLine());
+            //Reading choice
+            Console.WriteLine("1.Addition 2.Subtraction 3.Multiplication 4.Division");
+            Console.WriteLine("Enter choice");
+            ch= Convert.ToInt32(Console.ReadLine());
+            if (ch== 1)
+                Console.WriteLine(cal.Add(op1,op2));
+            if (ch == 2)
+                Console.WriteLine(cal.Subtract(op1, op2));
+            if (ch == 3)
+                Console.WriteLine(cal.Multiply(op1, op2));
+            if (ch == 4)
+                Console.WriteLine(cal.Divide(op1, op2));
+        }
+    }
+}
